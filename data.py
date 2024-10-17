314cb645f4219da4034de36599690af6e8dbeb3f
Tobias Kaiser <tobias.kaiser@student.kit.edu>
2024-10-18 01:47:36 +0200

    add format file option

--- data.py
+++ data.py
@@ -6,26 +6,36 @@ from huggingface_hub import InferenceClient
 bearer_token = "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"
 
 prompt = """Please create a short medical report only including sections about Patient
-        History, Clinical Findings + Diagnosis and Treatment Options based on this Patient description: """
+        History, Clinical Findings + Diagnosis and Treatment Options based on this Patient description: {}"""
+prompt_with_format = """Please create a short medical report with the following structure {},
+        based on the following patient description: {}"""
 max_tokens = 700
 
 def valid_args():
     args = parser.parse_args()
     try:
-        file = open(args.patient_desc, "r")
+        desc_file = open(args.patient_desc, "r")
+        if args.report_format:
+            format_file = open(args.report_format, "r")
     except:
         print("Please provide a valid file path")
     else:
-        return file.read()
+        return (desc_file.read(), format_file.read()) if args.report_format else (desc_file.read(), "")
 
-def inference(patient_desc: str):
+
+def inference(patient_desc: str, format: str):
     client = InferenceClient("mistralai/Mistral-Nemo-Instruct-2407", token=bearer_token)
-    res = client.text_generation(prompt + patient_desc, max_new_tokens=max_tokens)
-    print(res)
+    if format == "":
+        res = client.text_generation(prompt.format(patient_desc), max_new_tokens=max_tokens)
+        print(res)
+    else:
+        res = client.text_generation(prompt=prompt_with_format.format(format, patient_desc), max_new_tokens=max_tokens)
+        print(res)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="mv_challenge", description="CLI for automatically creating medical reports based on patient descriptions")
     parser.add_argument("patient_desc", type=str, help="path to patient description as txt file")
-    patient_desc = valid_args()
-    inference(patient_desc)
+    parser.add_argument("--report_format", type=str, help="path to txt file containing wanted report format")
+    patient_desc, format = valid_args()
+    inference(patient_desc, format)
